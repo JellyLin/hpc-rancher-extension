c4a2cb93ed197c147890b41aa2735a6549359052
Jelly Lin <johndw22@gmail.com>
2024-04-19 16:54:19 +0800

    Revert "C2 #118654 [Rancher] add HPC Management UI"

--- pkg/jelly-demo/models/app.js
+++
@@ -1,7 +0,0 @@
-function ContainerViewModel(data) {
-    this.Id = data.Id;
-    this.icon = data.logo;
-    this.repoName = data.app_name;
-    this.chartNameDisplay = data.title;
-    this.chartDescription = data.description;
-}
