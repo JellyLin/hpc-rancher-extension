206e86e54c5f174153bf4a07aaaf21a14360cfb0
Jelly.Lin <Jelly.Lin@infortrend.com>
2024-04-23 13:08:46 +0800

    hello world

---
+++ pkg/jelly-clock/models/app.js
@@ -0,0 +1,7 @@
+function ContainerViewModel(data) {
+    this.Id = data.Id;
+    this.icon = data.logo;
+    this.repoName = data.app_name;
+    this.chartNameDisplay = data.title;
+    this.chartDescription = data.description;
+}
