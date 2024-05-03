9bdc954f529153a6ba99aed1504de7ef98c2bdd7
Jelly.Lin <Jelly.Lin@infortrend.com>
2024-05-03 11:10:26 +0800

    clean code

--- pkg/hpc-ui/routing/extension-routing.ts
+++ pkg/hpc-ui/routing/extension-routing.ts
@@ -1,8 +1,4 @@
 // ./routing/extension-routing.ts
-import ListResource from '@shell/pages/c/_cluster/_product/_resource/index.vue';
-import CreateResource from '@shell/pages/c/_cluster/_product/_resource/create.vue';
-import ViewResource from '@shell/pages/c/_cluster/_product/_resource/_id.vue';
-import ViewNamespacedResource from '@shell/pages/c/_cluster/_product/_resource/_namespace/_id.vue';
 import HpcAppCenter from '../pages/HpcAppCenter.vue';
 import HpcJobTable from '../list/batch.volcano.sh.job.vue';
 
@@ -11,9 +7,7 @@ import HpcJobTable from '../list/batch.volcano.sh.job.vue';
 // so that the developer can import it wherever it needs to be used
 const YOUR_PRODUCT_NAME = 'HPC';
 const CUSTOM_PAGE_NAME = 'VolcanoJobs';
-const CUSTOM_PAGE_NAME_2 = 'File explorer';
-const CUSTOM_PAGE_NAME_3 = 'Summary';
-const CUSTOM_PAGE_NAME_4 = 'Settings';
+const CUSTOM_PAGE_NAME_2 = 'AppCenter';
 const YOUR_K8S_RESOURCE_NAME = 'batch.volcano.sh.job';
 
 const routes = [
@@ -42,44 +36,6 @@ const routes = [
     component: HpcAppCenter,
     meta:      { product: YOUR_PRODUCT_NAME },
   },
-  {
-    name:      `c-cluster-${ YOUR_PRODUCT_NAME }-${ CUSTOM_PAGE_NAME_3 }`,
-    path:      `/c/:cluster/${ YOUR_PRODUCT_NAME }/${ CUSTOM_PAGE_NAME_3 }`,
-    component: HpcAppCenter,
-    meta:      { product: YOUR_PRODUCT_NAME },
-  },
-  {
-    name:      `c-cluster-${ YOUR_PRODUCT_NAME }-${ CUSTOM_PAGE_NAME_4 }`,
-    path:      `/c/:cluster/${ YOUR_PRODUCT_NAME }/${ CUSTOM_PAGE_NAME_4 }`,
-    component: HpcAppCenter,
-    meta:      { product: YOUR_PRODUCT_NAME },
-  },
-  // // the following routes cover the "resource page"
-  // // registering routes for list/edit/create views
-  // {
-  //   name:      `c-cluster-${ YOUR_PRODUCT_NAME }-resource`,
-  //   path:      `/c/:cluster/${ YOUR_PRODUCT_NAME }/:resource`,
-  //   component: ListResource,
-  //   meta:      { product: YOUR_PRODUCT_NAME },
-  // },
-  // {
-  //   name:      `c-cluster-${ YOUR_PRODUCT_NAME }-resource-create`,
-  //   path:      `/c/:cluster/${ YOUR_PRODUCT_NAME }/:resource/create`,
-  //   component: CreateResource,
-  //   meta:      { product: YOUR_PRODUCT_NAME },
-  // },
-  // {
-  //   name:      `c-cluster-${ YOUR_PRODUCT_NAME }-resource-id`,
-  //   path:      `/c/:cluster/${ YOUR_PRODUCT_NAME }/:resource/:id`,
-  //   component: ViewResource,
-  //   meta:      { product: YOUR_PRODUCT_NAME },
-  // },
-  // {
-  //   name:      `c-cluster-${ YOUR_PRODUCT_NAME }-resource-namespace-id`,
-  //   path:      `/:cluster/${ YOUR_PRODUCT_NAME }/:resource/:namespace/:id`,
-  //   component: ViewNamespacedResource,
-  //   meta:      { product: YOUR_PRODUCT_NAME },
-  // }
 ];
 
 export default routes;
