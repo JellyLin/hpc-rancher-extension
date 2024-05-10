2d56446557911126ce22995cc9e69f5c058babb2
Jelly.Lin <Jelly.Lin@infortrend.com>
2024-05-10 17:56:36 +0800

    daily save

--- pkg/hpc-ui/list/HpcJobTable.vue
+++ pkg/hpc-ui/list/HpcJobTable.vue
@@ -1,5 +1,7 @@
 <script>
 import ResourceTable from '@shell/components/ResourceTable';
+import Masthead from '@shell/components/ResourceList/Masthead';
+import ButtonDropdown from '@shell/components/ButtonDropdown';
 import { STATE, NAME, AGE } from '@shell/config/table-headers';
 import { LOGGING } from '@shell/config/types';
 import ResourceFetch from '@shell/mixins/resource-fetch';
@@ -8,7 +10,11 @@ const YOUR_K8S_RESOURCE_NAME = 'batch.volcano.sh.job';
 
 export default {
   name:       'VolcanoJobs556',
-  components: { ResourceTable },
+  components: {
+    ResourceTable,
+    Masthead,
+    ButtonDropdown
+  },
   mixins:     [ResourceFetch],
   props:      {
     useQueryParamsForSimpleFiltering: {
@@ -17,12 +23,15 @@ export default {
     }
   },
 
-  resource: YOUR_K8S_RESOURCE_NAME,
-
   data() {
+    const inStore = this.$store.getters['currentProduct'].inStore;
+    // return !!this.$store.getters[`${ inStore }/schemaFor`](METRIC.NODE);
+    const schema = this.$store.getters[`${ inStore }/schemaFor`](YOUR_K8S_RESOURCE_NAME);
+
     return {
+      resource:  YOUR_K8S_RESOURCE_NAME,
       resources: [],
-      schema:    []
+      schema,
     };
   },
 
@@ -33,15 +42,15 @@ export default {
         NAME,
         {
           name:     'tasks',
-          labelKey: 'tableHeaders.tasks',
+          labelKey: 'hpc.tableHeaders.tasks',
           search:   ['tasks'],
-          value:    'tasks', // spec.tasks
-          getValue:  row => row.spec.tasks.length,
+          // value:    'tasks', // spec.tasks
+          getValue:  (row) => row.spec.tasks.length,
         },
         {
           name:      'containers',
-          labelKey:  'tableHeaders.containers',
-          value:     'containers', // .spec.tasks[].template.spec.containers[]
+          labelKey:  'hpc.tableHeaders.containers',
+          // value:     'containers', // .spec.tasks[].template.spec.containers[]
           width:     130,
           getValue:  (row) => {
             let cnt = 0;
@@ -55,42 +64,58 @@ export default {
         },
         {
           name:      'plugins',
-          labelKey:  'tableHeaders.plugins',
+          labelKey:  'hpc.tableHeaders.plugins',
           search:   ['plugins'],
-          value:     'plugins', // .spec.plugins
-          getValue:  row => row.plugins.length,
+          value:     'spec.plugins', // .spec.plugins
+          getValue:  (row) => {
+            return row.plugins.length;
+          },
         },
         {
           name:      'volumes',
-          labelKey:  'tableHeaders.volumes',
+          labelKey:  'hpc.tableHeaders.volumes',
           search:   ['.spec.volumes'],
-          value:     '.spec.volumes',
+          value:     'spec.volumes',
         },
         {
           name:      'runningDuration',
-          labelKey:  'tableHeaders.runningDuration',
-          value:     '.status.runningDuration',
+          labelKey:  'hpc.tableHeaders.runningDuration',
+          value:     'status.runningDuration',
         },
         {
           name:      'queue',
-          labelKey:  'tableHeaders.queue',
+          labelKey:  'hpc.tableHeaders.queue',
           search:   ['.spec.queue'],
-          value:     '.spec.queue',
+          value:     'spec.queue',
         },
       ];
 
       out.push(AGE);
 
       out.push({
-        name:  'console',
-        label: ' ',
-        align: 'right',
-        width: 65,
+        name:     'console',
+        labelKey: 'hpc.tableHeaders.open',
+        label:    ' ',
+        align:    'right',
+        width:    65,
       });
 
       return out;
     },
+    importLocation() {
+      return {
+        name:   'c-cluster-product-resource-create',
+        params: {
+          product:  this.$store.getters['currentProduct'].name,
+          resource: this.schema.id,
+        },
+      };
+    },
+    canCreateCluster() {
+      const schema = this.$store.getters['management/schemaFor'](YOUR_K8S_RESOURCE_NAME);
 
+      return !!schema?.collectionMethods.find((x) => x.toLowerCase() === 'post');
+    },
   //   filteredRows() {
   //     return this.rows;
   //   },
@@ -102,7 +127,7 @@ export default {
     this.$fetchType(LOGGING.CLUSTER_OUTPUT);
 
     this.resources = await this.$fetchType(YOUR_K8S_RESOURCE_NAME);
-    // this.schema = this.$store.getters['management/schemaFor'](YOUR_K8S_RESOURCE_NAME);
+    this.schema = this.$store.getters['management/schemaFor'](YOUR_K8S_RESOURCE_NAME);
     // this.schema = this.$store.getters['type-map/headersFor'](YOUR_K8S_RESOURCE_NAME);
     console.log(this);
   },
@@ -111,14 +136,57 @@ export default {
 
 <template>
   <div>
-    <Button>Button Here</Button>
-    <!-- :schema="schema" -->
+    <Masthead
+      :schema="schema"
+      :resource="resource"
+      :is-creatable="true"
+    >
+      <template
+        v-if="canCreateCluster"
+        slot="extraActions"
+      >
+        <n-link
+          :to="importLocation"
+          class="btn role-primary"
+        >
+          {{ t('cluster.importAction') }}
+        </n-link>
+      </template>
+    </Masthead>
     <ResourceTable
+      :schema="schema"
       :headers="headers"
       :rows="resources"
       :loading="loading"
       :use-query-params-for-simple-filtering="useQueryParamsForSimpleFiltering"
       :force-update-live-and-delayed="forceUpdateLiveAndDelayed"
-    />
+    >
+      <template
+        slot="cell:console"
+        slot-scope="scope"
+      >
+        <div class="name-console">
+          <LinkDetail
+            v-if="true"
+            v-model="scope.row.metadata.name"
+            :row="scope.row"
+          />
+          <span v-else>
+            {{ scope.row.metadata.name }}
+          </span>
+
+          <div class="overview-web-console">
+            <ButtonDropdown
+              button-label="Console"
+              size="sm"
+            />
+            <!-- :disabled="isOff"
+              :no-drop="isOff" -->
+            <!-- :dropdown-options="options" -->
+            <!-- @click-action="handleDropdown" -->
+          </div>
+        </div>
+      </template>
+    </ResourceTable>
   </div>
 </template>
