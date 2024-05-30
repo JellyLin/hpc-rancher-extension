f578f84c3035ebed845317234264fb73621e6eb2
Jelly.Lin <Jelly.Lin@infortrend.com>
2024-05-30 15:17:59 +0800

    hello world proxy to EonOne

--- pkg/hpc-ui/pages/FileExplorer.vue
+++ pkg/hpc-ui/pages/FileExplorer.vue
@@ -11,12 +11,14 @@ export const NODE_ROLES = {
   SCMGMT_IP:         'node-role.cluster/scmgmt-ip',
 };
 
+// use Connie API
 function getEonOneIp(store) {
-  const allNodes = store.getters['cluster/all'](NODE);
+  const inStore = store.getters['currentProduct'].inStore;
+  const allNodes = store.getters[`${ inStore }/all`](NODE);
   const headNode = allNodes.filter(n => n.metadata.labels[NODE_ROLES.HEAD_NODE]);
   const scmgmtIP = headNode[0]?.metadata?.annotations[NODE_ROLES.SCMGMT_IP];
 
-  return (scmgmtIP === '') ? scmgmtIP : '172.27.120.51';
+  return (scmgmtIP === '') ? scmgmtIP : '172.27.118.109';
 }
 
 export default {
@@ -25,20 +27,53 @@ export default {
 
   data() {
     const principal = this.$store.getters['rancher/byId']('principal', this.$store.getters['auth/principalId']);
-    const scmgmtIP = getEonOneIp(this.$store);
     const showExternalStorage = RaidCliService.showExternalStorage;
     const deleteMap = RaidCliService.deleteMap;
-    console.log(RaidCliService);
+    console.log(this);
 
     return {
       principal,
-      scmgmtIP,
+      scmgmtIP: '',
       RaidCliService,
       cmd: { showExternalStorage, deleteMap }
     };
   },
 
-  methods: {},
+  async fetch() {
+    const inStore = this.$store.getters['currentProduct'].inStore;
+
+    if (this.$store.getters[`${ inStore }/schemaFor`](NODE)) {
+      this.scmgmtIP = await getEonOneIp(this.$store);
+    }
+  },
+
+  methods: {
+    async test(cli) {
+      try {
+        console.log(cli);
+        // kubectl get cm 2ad5f604-7cff-48b6-b891-57882847f222
+        // apply configmap under user namespace
+        // cli: showExternalStorage, fill params
+        const requestOptions = {
+          url:                  `meta/proxy/http:/${ this.scmgmtIP }:8816/eonkube/job/2ad5f604-7cff-48b6-b891-57882847f222`,
+          method:               'GET',
+          headers:              { 'Content-Type': 'application/json' },
+          // redirectUnauthorized: false,
+        };
+
+        const response = await this.$store.dispatch('management/request', requestOptions);
+        const data = await response;
+
+        if (data.access_token !== undefined && data.access_token !== null && data.access_token !== '') {
+          return true;
+        }
+
+        return false;
+      } catch (e) {
+        return false;
+      }
+    }
+  },
 
   computed: {},
 };
@@ -60,11 +95,16 @@ export default {
       <li><a :href="`http://172.24.110.128:8989?token=abc&username=${principal.loginName}`" target="_blank">6. Redirect UI to GSx FileExplorer with token</a></li>
     </ul>
     <ul>
-      <li v-for="item in RaidCliService" :key="item">
+      <li v-for="(item, index) in RaidCliService" :key="index">
         {{ item }}
       </li>
-      <li v-for="item in cmd" :key="item">
-        {{ item }}
+      <li v-for="(item, index) in cmd" :key="index">
+        <div v-if="true">
+          <span>{{ item }}</span>
+          <button :action="test(item)">
+            Click
+          </button>
+        </div>
       </li>
     </ul>
   </div>
